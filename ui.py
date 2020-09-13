b3138cb2669305e82521f4256b6a0d787962f11d
bearitt <barret.jackson@gmail.com>
2020-09-13 16:54:20 -0700

    Update UI to include user choice for file output

--- ui.py
+++ ui.py
@@ -8,9 +8,30 @@ def user_search():
             stock_info = stock_lookup(symbol)
             for key in stock_info:
                 print(str(key) + ": " + str(stock_info[key]))
-            output_txt(stock_info)
-            output_json(stock_info)
-            output_csv(stock_info)
+            try:
+                user_choice = -1
+                while user_choice < 0 or user_choice > 3:
+                    print('''Would you like to output the results to a file?
+    0. No thank you
+    1. .txt file
+    2. .json file
+    3. .csv file''')
+                    user_choice = int(input('Your choice: '))
+                    if user_choice==0:
+                        continue
+                    elif user_choice==1:
+                        output_txt(stock_info)
+                        print('Text file generated in reports directory')
+                    elif user_choice==2:
+                        output_json(stock_info)
+                        print('JSON file generated in reports directory')
+                    elif user_choice==3:
+                        output_csv(stock_info) 
+                        print('CSV file generated in reports directory')
+                    else:
+                        print('Invalid choice, try again!')
+            except ValueError:
+                print('Invalid choice, try again!')
         except IndexError:
             print('Sorry, we couldn\'t find a company matching that symbol')
         print('-----------------------------')
